192d3844665504da37fac7a3b856e4d656ce88f3
Arthur Torres <arthur.torresdl03@gmail.com>
2024-10-08 13:52:27 -0300

    Pterodactyl added and better enemie spwan logic.
    New flying enemie was added (pterodactyl).
    Better enemie spawn logic (with timers) and difficulty increase based on the score.

--- gamecode.py
+++ gamecode.py
@@ -1,5 +1,6 @@
 import pygame
 from sys import exit
+from random import randint
 
 def display_score():
     current_time = pygame.time.get_ticks() - start_time
@@ -7,6 +8,25 @@ def display_score():
     score_rect = score_surface.get_rect(midbottom = (60, 30))
     screen.blit(score_surface, score_rect)
     return current_time//1000
+
+def enemie_movement(enemie_list):
+    if enemie_list:
+        for enemie_rect in enemie_list:
+            enemie_rect.x -= 5
+
+            if enemie_rect.bottom == 150: screen.blit(dino_surface, enemie_rect)
+            else: screen.blit(pterodactyl_surface, enemie_rect)
+
+        enemie_list = [enemie for enemie in enemie_list if enemie.x > -100]
+
+        return enemie_list
+    else: return []
+
+def collision(bimbo, enemies):
+    if enemies:
+        for enemies_rect in enemies:
+            if bimbo.colliderect(enemies_rect): return False
+    return True
 
 pygame.init()
 
@@ -32,10 +52,11 @@ bimbo_rect = bimbo_surface.get_rect(midbottom = (80, 150))
 bimbo_gravity = 0
 
 
-#Dino initial data
+#Enemies initial data
 dino_surface = pygame.image.load('sprites/enemies/dino.png').convert_alpha()
-dino_rect = dino_surface.get_rect(midbottom = (600, 150))
+pterodactyl_surface = pygame.image.load('sprites/enemies/pterodactyl.png').convert_alpha()
 
+enemies_rect_list = []
 
 #Bullet initial data
 bullet_surface = pygame.image.load('sprites/bullet.png').convert_alpha()
@@ -52,11 +73,16 @@ game_name_rect = game_name.get_rect(center = (300, 25))
 game_message = font.render("Press Space to RUN!", False, 'White')
 game_message_rect = game_message.get_rect(center = (300, 175))
 
+#Timer
+enemie_timer = pygame.USEREVENT + 1
+spawn_gap = 1500
+pygame.time.set_timer(enemie_timer, spawn_gap)
 
 #Game Loop
 while True:
     #Actions
     for event in pygame.event.get():
+    
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
@@ -72,9 +98,14 @@ while True:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     running = True
-                    dino_rect.left = 600
                     #Previous Time (in case of restart)
                     start_time = pygame.time.get_ticks()
+
+        if event.type == enemie_timer and running == True:
+            if randint(0, 2):
+                enemies_rect_list.append(dino_surface.get_rect(midbottom = (randint(900, 1100), 150)))
+            else:
+                enemies_rect_list.append(pterodactyl_surface.get_rect(midbottom = (randint(900, 1100), 75)))
 
     if running:
 
@@ -92,21 +123,30 @@ while True:
             bimbo_surface = pygame.image.load('sprites/bimbo/bimbo_jump.png').convert_alpha()
         screen.blit(bimbo_surface, bimbo_rect)
 
-        #Dino
-        dino_rect.right -= 5
-        if dino_rect.right < 0:
-            dino_rect.left = 600
-        screen.blit(dino_surface, (dino_rect))
+        #Enemies movement
+        enemies_rect_list = enemie_movement(enemies_rect_list)
 
         #Score
-        score = display_score() 
+        score = display_score()
+
+        #Difficulty speed
+        if score == 20:
+            spawn_gap = 1300
+        if score == 40:
+            spawn_gap = 1000
+        if score == 60:
+            spawn_gap = 800
+        if score == 80:
+            spawn_gap = 600
+        if score == 100:
+            spawn_gap = 400
 
         #Collision
-        if dino_rect.colliderect(bimbo_rect):
-            running = False
+        running = collision(bimbo_rect, enemies_rect_list)
 
     else:
         bimbo_rect.y = 150
+        enemies_rect_list.clear()
         screen.fill((147, 47, 55))
         screen.blit(bimbo_stand_surface, bimbo_stand_rect)
         screen.blit(game_name, game_name_rect)
